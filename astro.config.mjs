52eca5c654b01af03a367ce54d03ce1e27f5efd0
Handemore7 <handemore7@gmail.com>
2023-10-25 00:47:27 -0500

    test2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://Handemore7.github.io',
-  base: '/PDG-Camila',
+  /* base: '/PDG-Camila', */
 });
