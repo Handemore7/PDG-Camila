c0c095e82ed9ddd37ef875e5b68f75dfb49be04e
Handemore7 <handemore7@gmail.com>
2023-11-21 02:58:19 -0500

    cambios2

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,8 +1,10 @@
 import NavBar from "./NavBar.astro";
 import Footer from "./Footer.astro";
+import SeccionMain from "./SeccionMain.astro";
 
 const cosas = {
     NavBar,
-    Footer
+    Footer,
+    seccionMain
 }
 export default cosas
