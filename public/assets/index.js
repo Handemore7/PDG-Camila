32290e1f30a037c6411cd945a35c4c0ca7b57b13
Handemore7 <handemore7@gmail.com>
2023-11-27 11:59:32 -0500

    asd

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,7 +1,25 @@
 import logoBlanco from './logo-blanco.png'
 import logoNegro from './logo-negro.png'
+import navBar from './navBar.png'
+import fondoSeccion1 from './FondoSeccion1.png';
+import titulo from './titulo.png';
+import seccion2BG from './seccion2.png';
+import seccion2img from './seccion2-2.png'
+import seccion3Title from './seccion3-1.png'
+import seccion3img from './seccion3-2.png'
+import seccion3_2 from './seccion3-3.png'
+import seccion3_2title from './seccion3-4.png'
 
 export {
     logoBlanco,
-    logoNegro
+    logoNegro,
+    navBar,
+    fondoSeccion1,
+    titulo,
+    seccion2BG,
+    seccion2img,
+    seccion3Title,
+    seccion3img,
+    seccion3_2,
+    seccion3_2title
 }
