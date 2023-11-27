b689edf4c07139c937969882a68bb246ba9d9555
Handemore7 <handemore7@gmail.com>
2023-11-27 16:22:23 -0500

    qaz

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,4 +1,4 @@
-/* import logoBlanco from './logo-blanco.png'
+import logoBlanco from './logo-blanco.png'
 import logoNegro from './logo-negro.png'
 import navBar from './navBar.png'
 import fondoSeccion1 from './FondoSeccion1.png';
@@ -9,6 +9,8 @@ import seccion3Title from './seccion3-1.png'
 import seccion3img from './seccion3-2.png'
 import seccion3_2 from './seccion3-3.png'
 import seccion3_2title from './seccion3-4.png'
+import telaResistencia1 from './TelaResistencia1.png'
+import telaResistencia2 from './TelaResistencia2.png'
 
 export {
     logoBlanco,
@@ -21,5 +23,7 @@ export {
     seccion3Title,
     seccion3img,
     seccion3_2,
-    seccion3_2title
-} */
+    seccion3_2title,
+    telaResistencia1,
+    telaResistencia2
+}
