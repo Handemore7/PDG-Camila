4195e51b211dcfa9d979369bf61999b0da4e9ab3
Handemore7 <handemore7@gmail.com>
2023-11-27 17:33:59 -0500

    1234343

--- public/assets/index.js
+++ public/assets/index.js
@@ -11,6 +11,13 @@ import seccion3_2 from './seccion3-3.png'
 import seccion3_2title from './seccion3-4.png'
 import telaResistencia1 from './TelaResistencia1.png'
 import telaResistencia2 from './TelaResistencia2.png'
+import voces_seccion1_1 from './VocesSeccion1-1.png'
+import voces_seccion1_2 from './VocesSeccion1-2.png'
+import voces_seccion1_3 from './VocesSeccion1-3.png'
+import voces_seccion2 from './VocesSeccion2.png'
+import voces_seccion3_1 from './VocesSeccion3-1.png'
+import voces_seccion3_2 from './VocesSeccion3-2.png'
+import video from './video.png'
 
 export {
     logoBlanco,
@@ -25,5 +32,12 @@ export {
     seccion3_2,
     seccion3_2title,
     telaResistencia1,
-    telaResistencia2
+    telaResistencia2,
+    voces_seccion1_1,
+    voces_seccion1_2,
+    voces_seccion1_3,
+    voces_seccion2,
+    voces_seccion3_1,
+    voces_seccion3_2,
+    video
 }
