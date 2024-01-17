ae31a0782a949bbc115923d9ef422e931bd11822
Handemore7 <handemore7@gmail.com>
2024-01-17 05:42:47 -0500

    Mas tik toks

---
+++ src/Components/Test2.jsx
@@ -0,0 +1,21 @@
+import { TikTokEmbed } from 'react-social-media-embed';
+const Test2 = () => {
+  return (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '200vh',
+      margin: 'auto'
+    }}
+  >
+    <TikTokEmbed
+      url="https://www.tiktok.com/@camilaparedeso_/video/7323242492001045766"
+      width={325}
+      
+    />
+  </div>
+  )
+}
+
+export default Test2
---
+++ src/Components/Test3.jsx
@@ -0,0 +1,21 @@
+import { TikTokEmbed } from 'react-social-media-embed';
+const Test3 = () => {
+  return (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '200vh',
+      margin: 'auto'
+    }}
+  >
+    <TikTokEmbed
+      url="https://www.tiktok.com/@camilaparedeso_/video/7323781849183177989"
+      width={325}
+      
+    />
+  </div>
+  )
+}
+
+export default Test3
