6098f461bdc41febeb4cfc4e85a4f8e03875f7b6
Handemore7 <handemore7@gmail.com>
2024-01-17 04:53:54 -0500

    Tik tok embed

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-
+import { TikTokEmbed } from 'react-social-media-embed';
 const Test = () => {
   return (
-    <div className='bg-[red]'>
-        <blockquote
-            className='tiktok-embed'
-            cite='https://www.tiktok.com/@camilaparedeso_/video/7322294350971407621'
-            data-video-id='7322294350971407621'
-            
-        >
-            <section class=''></section>
-        </blockquote>
-        <script async src='https://www.tiktok.com/embed.js'></script>
-    </div>
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '200vh'
+    }}
+  >
+    <TikTokEmbed
+      url="https://www.tiktok.com/@camilaparedeso_/video/7322294350971407621"
+      width={325}
+      
+    />
+  </div>
   )
 }
 
